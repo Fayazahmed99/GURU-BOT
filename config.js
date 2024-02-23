40a3822d29d464e8ba9d4c1765cfab398cf257a9
Fayaz Ahmed <118333957+fayazahmedwazir@users.noreply.github.com>
2024-02-23 12:17:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['923029810479', 'AHMED 💀', true],
-  ['923029810479', 'AHMED 💀', true], 
-  ['923029810479'] 
+  ['923366391407', 'Fayaz | Ahmed ☑️', true],
+  ['923029810479', 'Fayaz | Ahmed ☑️', true], 
+  [''] 
 ] //Number of owners
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "923366391407" //put your bot number here
 
-global.mods = ['923029810479','32460248586'] 
-global.prems = ['923029810479', '32460248586', '919398758484']
-global.allowed = ['923029810479', '32460248586', '19152999993']
+global.mods = ['923366391407','923029810479'] 
+global.prems = ['923029810479', '923366391407', '919398758484']
+global.allowed = ['923029810479', '923366391407', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'AHMED Bot 💀'
+global.botname = 'Fayaz | Ahmed ☑️'
 global.premium = 'true'
-global.packname = 'AHMED 💀' 
-global.author = '@Asliguru' 
+global.packname = 'Fayaz | Ahmed ☑️' 
+global.author = 'Fayaz | Ahmed ☑️' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
@@ -64,7 +64,7 @@ global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
 
-global.wait = '*⌛ _Loading_please_Wait..._*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
